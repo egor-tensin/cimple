c6d109d477a16ca39027cd624dfd3517bfdcc5e0
Egor Tensin <Egor.Tensin@gmail.com>
2023-04-27 20:43:30 +0200

    ci.sh: build w/ gcc & clang

--- ci.sh
+++ ci.sh
@@ -3,4 +3,29 @@
 set -o errexit -o nounset -o pipefail
 shopt -s inherit_errexit lastpipe
 
-docker-compose build --progress plain --pull
+test_build() {
+    docker-compose build --progress plain --pull "$@"
+}
+
+test_build_clang() {
+    echo ----------------------------------------------------------------------
+    echo Building w/ clang
+    echo ----------------------------------------------------------------------
+    echo
+    test_build --build-arg C_COMPILER=clang
+}
+
+test_build_gcc() {
+    echo ----------------------------------------------------------------------
+    echo Building w/ gcc
+    echo ----------------------------------------------------------------------
+    echo
+    test_build --build-arg C_COMPILER=gcc
+}
+
+main() {
+    test_build_clang
+    test_build_gcc
+}
+
+main
