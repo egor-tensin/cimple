8d63cf5fe644162fac172214aa8cc00ddda99761
Egor Tensin <egor@tensin.name>
2026-06-12 17:44:39 +0200

    add .git-blame-ignore-revs

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -6,11 +6,26 @@ shopt -s inherit_errexit lastpipe
 script_dir="$( dirname -- "${BASH_SOURCE[0]}" )"
 script_dir="$( cd -- "$script_dir" && pwd )"
 
-cd -- "$script_dir"
+setup_clang_format_hook() {
+	local gitdir
+	gitdir="$( git rev-parse --git-dir )"
+	local hooks_dir
+	hooks_dir="$gitdir/hooks"
 
-gitdir="$( git rev-parse --git-dir )"
-hooks_dir="$gitdir/hooks"
+	local symlink_dest
+	symlink_dest="$( realpath "--relative-to=$hooks_dir" -- "$script_dir/pre-commit.sh" )"
 
-symlink_dest="$( realpath "--relative-to=$hooks_dir" -- "$script_dir/pre-commit.sh" )"
+	ln -fs -- "$symlink_dest" "$hooks_dir/pre-commit"
+}
 
-ln -fs -- "$symlink_dest" "$hooks_dir/pre-commit"
+setup_git_blame() {
+	git config blame.ignoreRevsFile .git-blame-ignore-revs
+}
+
+main() {
+	cd -- "$script_dir"
+	setup_clang_format_hook
+	setup_git_blame
+}
+
+main
